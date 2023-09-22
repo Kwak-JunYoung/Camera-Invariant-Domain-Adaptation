de567270f7cb78b2510ed048a321a70bd0b7567b
Kwak-JunYoung <klop9090@g.skku.edu>
2023-09-22 13:24:38 +0900

    Feat: Implementation of a function that gets the resulting info

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from models.unet import UNet
+from accelerate import Accelerator
+from torch.utils.data import Dataset, DataLoader
+from data_loaders import CustomDataset
+import numpy as np
 
 # Obtain information of the model
 def get_model_info(config, model_name):
@@ -9,3 +13,38 @@ def get_model_info(config, model_name):
         raise NotImplementedError("model name is not valid")
     return model_config, model
 
+def get_data_loader(accelerator):
+    accelerator.prepare()
+
+
+# Get test results to record in wandb
+def get_print_args(test_aucs, test_accs, config, train_config, model_config):
+    model_name = config.model_name
+    data_name = config.data_name
+
+    test_auc = np.mean(test_aucs)
+    test_auc_std = np.std(test_aucs)
+    test_acc = np.mean(test_accs)
+    test_acc_std = np.std(test_accs)
+
+    print("\n5-fold CV Result")
+    print("AUC\tACC\tRMSE")
+    print("{:.5f}\t{:.5f}".format(test_auc, test_acc))
+    
+    print_args = dict()
+    print_args["auc"] = round(test_auc, 4)
+    print_args["auc_std"] = round(test_auc_std, 4)
+    print_args["acc"] = round(test_acc, 4)
+    print_args["acc_std"] = round(test_acc_std, 4)
+
+    print_args['Model'] = model_name 
+    print_args['Dataset'] = data_name 
+    print_args.update(train_config)
+    print_args.update(model_config)
+
+    return print_args
+
+
+def main():
+    dataset = CustomDataset(csv_file='./train_source.csv')
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=True, num_workers=4)
