70f22aaf7a7a172aa2e19b4518695c5bf48bd936
Kwak-JunYoung <klop9090@g.skku.edu>
2023-09-22 13:11:40 +0900

    Feat: Implementation of function that gets information of a model.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from models.unet import UNet
+
+# Obtain information of the model
+def get_model_info(config, model_name):
+    if model_name == "unet":
+        model_config = config["models"]["unet"]
+        model = UNet()
+    else: 
+        raise NotImplementedError("model name is not valid")
+    return model_config, model
+
