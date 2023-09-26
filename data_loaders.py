a91b21ebdf1d47765dcac14f54b04dc2e66f6086
Kwak-JunYoung <klop9090@g.skku.edu>
2023-09-26 10:59:53 +0900

    Feat: Implementation of various data augmentations.

--- data_loaders.py
+++ data_loaders.py
@@ -3,6 +3,7 @@ from albumentations.pytorch import ToTensorV2
 from torch.utils.data import Dataset
 import cv2
 import pandas as pd
+from transform import MyToTensor, MyNormalization, RandomFlip
 
 
 class CustomDataset(Dataset):
@@ -11,11 +12,23 @@ class CustomDataset(Dataset):
 
         self.data = pd.read_csv(csv_file)
 
+        self.to_tensor = MyToTensor()
+        self.normalize = MyNormalization()
+        self.flip = RandomFlip()
         self.transform = A.Compose(
             [
-                A.Resize(224, 224),
-                A.Normalize(),
-                ToTensorV2()
+                A.one_of([
+                    A.vertical_flip(p=0.5),
+                    A.horizontal_flip(p=0.5),
+                    A.RandomRotate90(p=1),
+                    A.Transpose(p=0.5),
+                ], p=0.5),
+
+                A.one_of([
+                    A.ElasticTransform(p=1, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
+                    A.GridDistortion(p=0.5),
+                    A.OpticalDistortion(distort_limit=2, shift_limit=0.5, p=1),
+                ], p=0.8),
             ]
         )
         
@@ -30,9 +43,8 @@ class CustomDataset(Dataset):
         image = cv2.imread(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        if self.infer:
-            if self.transform:
-                image = self.transform(image=image)['image']
+        if self.infer and self.transform:
+            image = self.transform(image=image)['image']
             return image
 
         mask_path = self.data.iloc[idx, 2]
